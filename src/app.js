fd286ab71e1b459181d10ef8b63f41f3bdf2ee3d
Maria Jose Tejada <tejada003@gmail.com>
2023-05-16 12:06:12 -0400

    servidor de correo completado

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const cors = require("cors")
 const app = express();
+const router = require("./routers/mailSender");
+ 
 const port = process.env.PORT || 3000;
+
+app.use(express.json);
+app.use(express.urlencoded({extended: true}));
+
+app.use(cors());
+app.use("/api/mail", router);
+
+app.get("/", (req, res)=>{
+    res.send("Pagina principal");
+})
 
 app.listen(port, ()=>{
     console.log("Servidor corriendo en el puerto", port);
